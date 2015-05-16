96df7fc10d9a70df04d3648d5e98246ba29cae6d
aditya <dev@chimeracoder.net>
2015-05-16 17:19:45 -0400

    Add sample extension that adds Visit Mozilla button

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var self = require('sdk/self');
+var buttons = require('sdk/ui/button/action');
+var tabs = require("sdk/tabs");
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
+var button = buttons.ActionButton({
+    id: "mozilla-link",
+    label: "Visit Mozilla",
+      icon: {
+            "16": "./icon-16.png",
+            "32": "./icon-32.png",
+                "64": "./icon-64.png"
+                    },
+        onClick: handleClick
+});
+
+function handleClick(state) {
+    tabs.open("http://www.mozilla.org/");
 }
-
-exports.dummy = dummy;
